3f28930d1b741f76720be66ad269c006370ad7b6
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-30 23:02:06 +0530

    Added UC2 User registration validation of last name

--- src/com/bridgelabz/UserInputValidation.java
+++ src/com/bridgelabz/UserInputValidation.java
@@ -4,17 +4,23 @@ package com.bridgelabz;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserInputValidation 
-{
+public class UserInputValidation {
 
-	private Pattern pattern;
-	private Matcher matcher;
+    private Pattern pattern;
+    private Matcher matcher;
 
-	public boolean isFirstNameValid(String firstName){
-		String regex = "[A-Z][a-z]{2,}";
-		pattern = Pattern.compile(regex);
-		matcher = pattern.matcher(firstName);
-		return matcher.matches();
-	}
+    public boolean isFirstNameValid(String firstName){
+        String regex = "[A-Z][a-z]{2,}";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+
+    public boolean isLastNameValid(String lastName){
+        String regex = "[A-Z][a-z]{2,}";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
 }
-
--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -3,20 +3,26 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class UserRegistration {
-	public static void main(String[] args) {
-		System.out.println("User registration problem");
-		Scanner sc = new Scanner(System.in);
-		UserInputValidation validation = new UserInputValidation();
+    public static void main(String[] args) {
+        System.out.println("User registration problem");
+        Scanner sc = new Scanner(System.in);
+        UserInputValidation validation = new UserInputValidation();
+        
+        System.out.println("Enter first name ");
+        String firstName = sc.next();
+        if (validation.isFirstNameValid(firstName))
+            System.out.println("Valid first name");
+        else
+            System.out.println("Invalid first name");
 
-		System.out.println("Enter first name ");
-		String firstName = sc.next();
-		if (validation.isFirstNameValid(firstName))
-			System.out.println("Valid first name");
-		else
-			System.out.println("Invalid first name");
+        System.out.println("Enter last name ");
+        String lastName = sc.next();
+        if (validation.isLastNameValid(lastName))
+            System.out.println("Valid last name");
+        else
+            System.out.println("Invalid last name");
 
 
-
-	}
+    }
 }
 
